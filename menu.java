b9aeea564e37132db9d6d16d038aa22353e842b2
cherreesims <145623229+cherreesims@users.noreply.github.com>
2023-09-28 19:26:53 -0500

    Update menu.java

--- menu.java
+++ menu.java
@@ -1,2 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+  class MenuExample {
+    private JFrame frame;
+    private JTextArea textArea;
+    private String RandomHue;
+
+    MenuExample() {
+      frame = new JFrame(title: "Menu");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setSize(width:600, height:400);
+      frame.setLayout(new BorderLayout());
